28aa283e85362268595148e27f0eac92eca8ccb5
oraclesean <oracle.sean@gmail.com>
2022-04-01 23:22:22 +0000

    Set the AU jar to the target home if not present in the AU directory

--- autoupgrade_run.sh
+++ autoupgrade_run.sh
@@ -1,5 +1,9 @@
 # Get the target database home:
 TARGET_HOME=${ORACLE_21C_HOME:-$ORACLE_19C_HOME}
+  if [ -f $ORADATA/autoupgrade.jar ]
+then AU_JAR=$ORADATA/autoupgrade.jar
+else AU_JAR=$TARGET_HOME/rdbms/admin/autoupgrade.jar
+fi
 
 # Store values for ORACLE_PATH and SQLPATH:
 __oracle_path=$ORACLE_PATH
@@ -9,7 +13,7 @@ __sqlpath=$SQLPATH
 unset ORACLE_PATH
 unset SQLPATH
 
-$TARGET_HOME/jdk/bin/java -jar $ORADATA/autoupgrade.jar -config $ORADATA/autoupgrade/config.txt -mode $1
+$TARGET_HOME/jdk/bin/java -jar $AU_JAR -config $ORADATA/autoupgrade/config.txt -mode $1
 
 export ORACLE_PATH=$__oracle_path
 export SQLPATH=$__sqlpath
